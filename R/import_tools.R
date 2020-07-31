5b5c86e34cfb4e569efe8d431360a58774351983
Laufenberg <jared_laufenberg@fws.gov>
2020-07-31 11:28:51 -0800

    Add import_captures function

--- R/import_tools.R
+++ R/import_tools.R
@@ -6,10 +6,41 @@
 #'
 #' @return Data.frame
 
-import_tables <- function(full_file_path, table_name) {
+import_table <- function(full_file_path, table_name) {
     con <- RODBC::odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=",
                                            full_file_path,";"))
     df <- RODBC::sqlFetch(con, table_name)
     RODBC::odbcClose(con)
     return(df)
+}
+
+
+#' Compile Capture Tables from Site-specific NWBF Lynx Access Databases
+#'
+#' @param file_names Character vector of full file paths to Access databases including file name
+#' @param telonics Logical value indicating whether to only compile records for Telonics collars. Default is TRUE.
+#' @param sites Character vector of site codes used to identify Access database file names to be used.
+#'
+#' @return Data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_captables("tet_lynx_capture_database_202000101.accdb")}
+
+import_captures <- function(file_names, telonics = TRUE, sites = c("kan", "kuk", "tet", "wsm", "ykf")){
+  siteids <- unlist(sapply(sites, grep, x = file_names))
+  cap_tables <- lapply(file_names, import_tables, table_name = "captures")
+  names(cap_tables) <- names(siteids)
+  cap_dat <- do.call("rbind", mapply(function(table, site){
+    table$Capture_Site = toupper(site )
+    return(table)
+  },
+  table = cap_tables, site = names(siteids), SIMPLIFY = FALSE))
+  cap_dat <- cap_dat[is.na(cap_dat$Den_ID),] # remove records w/ den IDs (newborn kittens)
+  if(telonics){
+    cap_dat <- cap_dat[cap_dat$Removed_Collar_Make %in% "Telonics" | cap_dat$Collar_Make %in% "Telonics",]
+  }
+  cap_dat <- refactor(cap_dat[order(cap_dat$Capture_Date),])
+  return(cap_dat)
 }
